afbe97b0112609dc7ee6490f57740c53da51a878
Estankov <52861411+Estankov@users.noreply.github.com>
2023-09-18 22:29:53 -0300

    morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const morgan = require('morgan')
+
 // routes
 const bootcamps = require('./routes/bootcamps')
 
@@ -9,9 +11,14 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Dev logger middleware
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
+
+
 // mount routes
 app.use('/api/v1/bootcamps', bootcamps);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(
