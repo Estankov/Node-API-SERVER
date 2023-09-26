bb7af289cdb9aaab360c09fbf7c2ab9b8b1fa7d2
Estankov <52861411+Estankov@users.noreply.github.com>
2023-09-25 22:56:04 -0300

    Cascade delete and Create courses

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -27,7 +27,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
     // Finding resource
-    query = Bootcamp.find(JSON.parse(queryStr));
+    query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
     // Select fields
     if(req.query.select) {
@@ -86,7 +86,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 // @access   Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
     
-    const bootcamp = await Bootcamp.findById(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id).populate('courses');
     if(!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
@@ -136,17 +136,18 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @access   Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
+
+    bootcamp.deleteOne();
     
     res.status(200).json({
         sucess: true,
         data: {}
     })
-
 
 });
 
