bfcfb23ff641e656253b02232f39332f2d63494f
Estankov <52861411+Estankov@users.noreply.github.com>
2023-09-27 11:07:29 -0300

    photo upload

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,6 +6,7 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload
 } = require("../controllers/bootcamps");
 
 // Include other resource routers
@@ -16,8 +17,9 @@ const router = express.Router();
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/radius/:zipcode/:distance')
-    .get(getBootcampsInRadius);
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router.route('/:id/photo').put(bootcampPhotoUpload);
 
 router.route("/")
     .get(getBootcamps)
