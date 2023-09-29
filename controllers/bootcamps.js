8df70ecaa4f9670fc77e9d45828eb8d8d57c889f
Estankov <52861411+Estankov@users.noreply.github.com>
2023-09-28 23:23:08 -0300

    middleware response advanced results

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,75 +9,7 @@ const geocoder = require('../utils/geocoder');
 // @access   Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
-    let query;
-
-    // Copy req.query
-    const reqQuery =  {...req.query };
-
-    // fields to exlude
-    const removeFields = ['select', 'sort', 'page', 'limit'];
-
-    // Loop over removeFields and delete them from reqQuery
-    removeFields.forEach(param => delete reqQuery[param]);
-
-    // Create query string
-    let queryStr = JSON.stringify(reqQuery);
-
-    // Create operator($gt, $gte, etc)
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-
-    // Finding resource
-    query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
-
-    // Select fields
-    if(req.query.select) {
-        const fields = req.query.select.split(',').join(' ');
-        query = query.select(fields);
-    }
-
-    // Sort
-    if(req.query.sort) {
-        const sortBy = req.query.sort.split(',').join(' ');
-        query = query.sort(sortBy);
-    } else {
-        query = query.sort('-createAt');
-    }
-
-    // Pagination
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 25;
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const total = await Bootcamp.countDocuments();
-
-    query = query.skip(startIndex).limit(limit);
-
-    // Executing query
-    const bootcamps = await query;
-
-    // Pagination result
-    const pagination =  {};
-
-    if(endIndex < total) {
-        pagination.next = {
-            page: page+1,
-            limit
-        }
-    }
-
-    if(startIndex > 0) {
-        pagination.prev = {
-            page: page-1,
-            limit
-        }
-    }
-
-    res.status(200).json({
-        sucess: true, 
-        count: bootcamps.length,
-        pagination,
-        data: bootcamps
-    });
+    res.status(200).json(res.advancedResults);
 
 });
 
@@ -220,13 +152,11 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
         }
         await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
 
-        res.status(200).json({
-            sucess: true,
-            data: file.name
-        });
-
     });
 
-
+    res.status(200).json({
+        sucess: true,
+        data: file.name
+    });
 
 });
