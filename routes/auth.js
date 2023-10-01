3147adee2a7901f7825123a45ee062b243ee6b37
Estankov <52861411+Estankov@users.noreply.github.com>
2023-10-01 16:07:25 -0300

    protect routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { register, loginUser } = require('../controllers/auth');
+const { register, loginUser, getMe } = require('../controllers/auth');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', loginUser);
+router.get('/me', protect,  getMe);
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -4,12 +4,14 @@ const { getCourses, getCourse, addCourse, updateCourese, deleteCourse } = requir
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
 
+const { protect, authorize } = require('../middleware/auth');
+
 const router = express.Router({ mergeParams: true});
 
 router.route('/').get(advancedResults(Course, {
     path: 'bootcamp',
     select: 'name description'
-}), getCourses).post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourese).delete(deleteCourse);
+}), getCourses).post(protect, authorize('publisher', 'admin'), addCourse);
+router.route('/:id').get(getCourse).put(protect, authorize('publisher', 'admin'), updateCourese).delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
