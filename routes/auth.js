ebb7cac1a1d1f6df15b1b8a51222cc1637926f44
estankov <nogueira116@gmail.com>
2023-10-13 12:33:27 -0300

    update user password and details

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, loginUser, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const { register, loginUser, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', loginUser);
 router.get('/me', protect,  getMe);
+router.put('/updatedetails', protect,  updateDetails);
+router.put('/updatepassword', protect,  updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
--- utils/geocoder.js
+++ utils/geocoder.js
@@ -2,7 +2,7 @@ const nodeGeocoder = require('node-geocoder');
 const options = {
     provider: process.env.GEOCODER_PROVIDER,
     httpAdapter: 'https',
-    apiKey: process.env.GEOCODER_APY_KEY,
+    apiKey: process.env.GEOCODER_API_KEY,
     formatter: null
 }
 
